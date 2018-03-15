9baf1d6ed44df18d010dfd1a064d95214a1265d7
Brahm Capoor <brahmcapoor@gmail.com>
2018-03-15 11:08:24 -0700

    Added readme

---
+++ CS106A/ContActivist/README.md
@@ -0,0 +1,22 @@
+# FlightPlanner!
+
+Download the eclipse project (including the web demo) [here]()
+
+## Summary
+
+This is a program based on the section 9 handout of CS 106A Winter Quarter 2018 at Stanford University. The genuinely important things are as follows:
+
+* `ContActivistClient.java`: a fully featured client
+* `ContActivistServer.java`: a server that students need to implement
+* `CongressMember.java`: a class representing a Congress Member that students need to implement.
+* `ContActivistServerSoln.java`: solution code for the server (to be posted after I teach section)
+* `CongressMemberSolution.java` solution code for the Congress Member class (to be posted after I teach section)
+
+The `webpage` directory has resources for a web client to show students for fun. At this point, I can't think of anything else that might be important but this document will be uploaded as necessary. 
+
+## How to use the web demo
+
+1) Download the Eclipse project from this repo (see the link at the top of this document) and import it as you normally would.
+2) Run `ContActivistServer.java` or `ContActivistServerSoln.java` from Eclipse, making sure that `private static final int PORT = 8000;`
+3) Open `webpage/index.html` in your browser.
+4) The demo should just work now. If anything weird comes up, shoot me an email at brahm@stanford.edu. 
--- CS106A/ContActivist/webpage/main.js
+++ CS106A/ContActivist/webpage/main.js
@@ -104,7 +104,7 @@ async function getRepresentatives() {
     mimeType: 'text/plain; charset=x-user-defined',
     url: url,
     success: e => updatePane(e),
-    error: e => console.log(e),
+    error: noServer,
     dataType: "text"
   });
 }
@@ -115,14 +115,72 @@ function clickHandler(event, data) {
   getRepresentatives();
 }
 
-$(document).ready(function() {
-  $('#map').usmap({
-    stateStyles: {fill: 'teal'},
-    stateHoverStyles: {fill: 'palegreen'},
-    stateHoverAnimation: 300,
-    stroke: '#ff0000',
-    click: clickHandler
+function noServer() {
+  console.log("bruh");
+  swal({
+    type:"error",
+    title: "Uh oh! It doesn't look like you have a server running",
+    html: "<p>Make sure you've started a server and that <code>private static final int PORT = 8000;</code></p>",
+    allowOutsideClick: false
+  }).then((result) => {
+    getRepresentatives();
   });
+}
+
+$(document).ready(function() {
+  swal({
+    type: "info",
+    title: "Welcome to ContActivist!",
+    html: `
+          <p> 
+            This is a sample webpage that allows you to see how a server that 
+            you made for a CS106A section might be used in a real website to find out how to
+            contact your state representatives. Before you get started, there are a couple of 
+            things you need to do: 
+          </p>
+          <ol>
+            <li> 
+              Download the eclipse project here. 
+              This project includes starter code (<code>ContActivistServer.java</code> and <code>CongressMember.java</code>), a solution (<code>ContActivistServerSolution.java</code> and <code>CongressMemberSolution.java</code>) and a 
+              fully-functioning client (<code>ContActivistClient.java</code>). It also includes a <code> webpage </code> directory, which is the source of this website.
+              You shouldn't need to worry about that directory, although you should feel free to peruse the code. 
+            </li>
+            <br>
+            <li>
+              Import the project into your eclipse workspace just as you would for your assignment starter code.
+            </li>
+            <br>
+            <li>
+              If you want the practice (and I think it's a great idea), try and implement the server according to the 
+              <a href="http://web.stanford.edu/class/cs106a/section/9/Section9.pdf"> handout </a> in <code>ContActivistServer.java</code> and
+              <code>CongressMember.java</code>.
+            </li>
+            <br>
+            <li>
+              Next, run your server. This would be either <code>ContActivistServer.java</code>, which you just wrote, or 
+              <code>ContActivistServerSolution.java</code> if you just want to try it out. 
+            </li>
+            <br>
+            <li>
+              Now, you should be ready to start playing around on this webpage. Try clicking different states and buttons 
+              and seeing how your server responds to them. Make sure to check your server logs if anything goes wrong. 
+            </li>
+          </ol>
+          `,
+    confirmButtonText: "I've started my server. Let's go!",
+    grow: "fullscreen",
+    allowOutsideClick: false
+  }).then((result) => {
+      if(result.value) {
+        $('#map').usmap({
+          stateStyles: {fill: 'teal'},
+          stateHoverStyles: {fill: 'palegreen'},
+          stateHoverAnimation: 300,
+          stroke: '#ff0000',
+          click: clickHandler
+        });
+      }
+  });  
 });
 
 $("#phoneButton").on('click', function () {
